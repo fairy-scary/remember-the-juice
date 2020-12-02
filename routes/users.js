e3f903b64bde974d01e07554b9ae0d8611f2248f
Angelica <angelica.wilson93@gmail.com>
2020-12-02 17:04:41 -0600

    Working on login functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,70 @@
 var express = require('express');
 var router = express.Router();
+const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const db = require('../db/models');
+
+const csrfProtection = csrf({ cookie: true });
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+const bcrypt = require('bcryptjs');
+
+// Login route
+router.get('/', csrfProtection, (req, res) => {
+  res.render('index', {
+    title: 'Login',
+    csrfToken: req.csrfToken(),
+  });
 });
 
+const loginValidators = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+
+// After 'login' button is clicked
+router.post('/', csrfProtection, loginValidators,
+  asyncHandler(async (req, res) => {
+    const { username, password } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: { username } });
+
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+        if (passwordMatch) {
+          return res.redirect('/');
+        }
+      }
+      errors.push('Login failed for the provided username and password. Please try again.');
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('index', {
+      title: 'Login',
+      username,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }));
+
+
+
+
+
+/* GET users listing. */
+// router.get('/', function (req, res, next) {
+//   res.send('respond with a resource');
+// });
 module.exports = router;
