0404bb15ce560e32bc8fc7f49a58362ab16fefe5
Angelica <angelica.wilson93@gmail.com>
2020-12-02 15:47:49 -0600

    initial auth branch commit

--- db/models/user.js
+++ db/models/user.js
@@ -2,11 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    hashedPassword: DataTypes.STRING.BINARY,
   }, {});
-  User.associate = function(models) {
-    User.hasMany(models.UserList, { foreignKey:'userId' });
-    User.hasMany(models.Task, { foreignKey:'userId' });
+  User.associate = function (models) {
+    User.hasMany(models.UserList, { foreignKey: 'userId' });
+    User.hasMany(models.Task, { foreignKey: 'userId' });
   };
   return User;
 };
