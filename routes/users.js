c6ba7d920da988164466f7ad1595115784f04e2d
Angelica <angelica.wilson93@gmail.com>
2020-12-03 14:32:12 -0600

    Work on pug. Also implement permissions

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const csrf = require('csurf');
 const { check, validationResult } = require('express-validator');
 
 const db = require('../db/models');
-const { loginUser } = require('../auth');
+const { loginUser, requireAuth } = require('../auth');
 
 const csrfProtection = csrf({ cookie: true });
 
@@ -27,29 +27,45 @@ const userValidators = [
   // Define the user validators.
   check('username')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Username')
+    .withMessage('Please provide a value for Username.')
     .isLength({ max: 50 })
-    .withMessage('Username must not be more than 50 characters long'),
+    .withMessage('Username must not be more than 50 characters long.')
+    .custom((value) => {
+      return db.User.findOne({ where: { username: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Username is already in use by another account. Please try again.');
+          }
+        })
+    }),
   check('email')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Email Address')
+    .withMessage('Please provide a value for Email Address.')
     .isLength({ max: 320 })
     .withMessage('Email Address must not be more than 320 characters long')
     .isEmail()
-    .withMessage('Email Address is not a valid email'),
+    .withMessage('Email Address is not a valid email. Please try again.')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account. Please try again.');
+          }
+        })
+    }),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Password')
+    .withMessage('Please provide a value for Password.')
     .isLength({ max: 50 })
-    .withMessage('Password must not be more than 50 characters long'),
+    .withMessage('Password must not be more than 50 characters long.'),
   check('confirmPassword')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Confirm Password')
+    .withMessage('Please provide a value for Confirm Password.')
     .isLength({ max: 50 })
-    .withMessage('Confirm Password must not be more than 50 characters long')
+    .withMessage('Confirm Password must not be more than 50 characters long.')
     .custom((value, { req }) => {
       if (value !== req.body.password) {
-        throw new Error('Confirm Password does not match Password');
+        throw new Error('Confirm Password does not match Password. Please try again');
       }
       return true;
     }),
@@ -74,8 +90,10 @@ router.post('/signup', csrfProtection, userValidators,
       const hashedPassword = await bcrypt.hash(password, 10);
       user.hashedPassword = hashedPassword;
       await user.save();
+      // Create Personal list for each new user
+      await db.UserList.create({ listName: 'Personal', userId: user.id });
       loginUser(req, res, user);
-      res.redirect(`/users/profile/:${user.id}`);
+      res.redirect(`/users/:${user.id}`);
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
       res.render('signup', {
@@ -88,6 +106,20 @@ router.post('/signup', csrfProtection, userValidators,
   }));
 
 
+// User profile page
+router.get(`/:id`, requireAuth, asyncHandler(async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = await db.User.findOne({ where: { id: userId } });
+
+  res.render('main', { user, })
+}));
+
+
+// Logout 
+router.post('/logout', (req, res) => {
+  logoutUser(req, res);
+  res.redirect('/');
+});
 
 
 
