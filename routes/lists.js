985e7c3461237cc9e83b3a7f601a134c036389ff
Angelica <angelica.wilson93@gmail.com>
2020-12-05 13:36:23 -0600

    Work to make delete lists functional

--- routes/lists.js
+++ routes/lists.js
@@ -7,7 +7,6 @@ const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).ca
 
 // Creates and saves a new list
 router.post('/', asyncHandler(async (req, res) => {
-  console.log(req.session)
   // if there's no req.session.auth (this would happen if the user hasn't logged in, thus would be in demo mode), then userId is hard coded to be demo's userId. 
   //Else, userId is received from session
   if (!req.session.auth) {
@@ -27,5 +26,16 @@ router.post('/', asyncHandler(async (req, res) => {
   }
 
 }));
+
+// Delete a list
+router.post('/delete', asyncHandler(async (req, res) => {
+  console.log(req.session)
+  const userId = req.session.auth.userId;
+  const { listName } = req.body
+  console.log(listName + '888888888888888888')
+  // UserList.destroy({where: {}})
+
+}));
+
 
 module.exports = router;
