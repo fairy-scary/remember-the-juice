6c9fe6892eca93ed749846ef5066988aa87b8da8
Angelica <angelica.wilson93@gmail.com>
2021-03-05 00:24:24 -0600

    Check iff correct https

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ const { restoreUser } = require('./auth');
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 // view engine setup
 app.set('view engine', 'pug');
 
